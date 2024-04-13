1c0a2e35cc62d703c3f04743c94b58d05c706f5c
bcarpenter03 <03benjamincarpenter@gmail.com>
2024-04-13 22:14:58 +0100

    Added API calling functionality

--- SDI Coursework.cpp
+++ SDI Coursework.cpp
@@ -2,30 +2,76 @@
 //
 
 #include <iostream>
-#include <curl/curl.h>
-#include <json/json.h> 
+
+#include<json/json.h>
+#include<curl/curl.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 
-// Callback function to handle response data
+struct WeatherData {// for storing weatherdata in a vector
+    int locationID;
+    std::string locationName;
+    std::string latitude;
+    std::string longitude;
+
+    //hourly variables
+    std::string temperature;
+    std::string relativeHumidity;
+    std::string precipitation;
+
+    std::string sealevelPressure;
+    std::string surfacePressure;
+    std::string cloudCover;
+
+    std::string windSpeed10;
+    /*
+    std::string windSpeed80;
+    std::string windSpeed120;
+    std::string windSpeed180;*/
+};
+
+void printWeatherData(const std::vector<WeatherData>& dataVector, int index) {
+    cout << "\nLocationID: " << dataVector[index].locationID;
+    cout << "\nLocation: " << dataVector[index].locationName;
+    cout << "\nLatitude: " << dataVector[index].latitude;
+    cout << "\nLongitude: " << dataVector[index].longitude;
+
+    cout << "\nTemperature: " << dataVector[index].temperature;
+    cout << "\nRelative Humidity: " << dataVector[index].relativeHumidity;
+    cout << "\nPrecipitation: " << dataVector[index].precipitation;
+
+    cout << "\nSea Level Pressure: " << dataVector[index].sealevelPressure;
+    cout << "\nSurface Pressure: " << dataVector[index].surfacePressure;
+    cout << "\nCloud Cover: " << dataVector[index].cloudCover;
+    cout << "\nWind Speed at 10 metres: " << dataVector[index].windSpeed10;
+    cout << "\n";
+};
+
+vector<string> commands{ "quit", "help", "addLocation", "removeLocation", "modifyLocation", "searchLocation", "favouriteLocation", "ShowAll", "TestAPI"};
+
 size_t writeCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
     output->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-int main() {
+std::vector<WeatherData> getWeatherData(string latitude, string longitude) {
     CURL* curl;
     CURLcode res;
     string responseBuffer;
 
-    // Initialize libcurl
+
     curl = curl_easy_init();
     if (curl) {
         // Set the URL you want to request
         string apiKey = "YOUR_API_KEY"; // Replace with your Open-Meteo API key
-        string url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+        string url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&current=temperature_2m&hourly=temperature_2m,relative_humidity_2m,precipitation,pressure_msl,surface_pressure,cloud_cover,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m&forecast_days=1";
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
-        // Set callback function to handle the response
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
 
@@ -42,14 +88,25 @@ int main() {
             bool parsingSuccessful = reader.parse(responseBuffer, root);
             if (parsingSuccessful) {
                 // Weather Attributes
+
                 string weatherLatitude = root["latitude"].asString();
-                string weatherTimezone = root["timezone"].asString();
-                string weatherTemperature = root["temperature_2m"].asString();
+                string weatherLongitude = root["longitude"].asString();
 
+                string hourlyTemperature = root["hourly"]["temperature_2m"][0].asString();
+                string hourlyHumidity = root["hourly"]["relative_humidity_2m"][0].asString();
+                string hourlyPrecipitation = root["hourly"]["precipitation"][0].asString();
+                string hourlySeaPressure = root["hourly"]["pressure_msl"][0].asString();
+                string hourlySurfacePressure = root["hourly"]["surface_pressure"][0].asString();
+                string hourlyCloudCover = root["hourly"]["cloud_cover"][0].asString();
+                string hourlyWindSpeed = root["hourly"]["wind_speed_10m"][0].asString();
 
-                cout << "Latitude: " << weatherLatitude << std::endl;
-                cout << "Timezone: " << weatherTimezone << std::endl;
-                cout << "Temperature: " << weatherTemperature << std::endl;
+                std::vector<WeatherData> TempVector;
+
+                TempVector.push_back({ 2, "Nottingham", weatherLatitude, weatherLongitude, hourlyTemperature,
+                    hourlyHumidity, hourlyPrecipitation, hourlySeaPressure, hourlySurfacePressure, hourlyCloudCover, hourlyWindSpeed });
+
+                return TempVector;
+
             }
             else {
                 cerr << "Failed to parse JSON response" << std::endl;
@@ -60,9 +117,74 @@ int main() {
     }
     else {
         std::cerr << "Failed to initialize libcurl" << std::endl;
-        return 1;
     }
 
-    return 0;
 }
+
+int main() {
+    cout << "WELCOME TO BEN'S WEATHER DATA APPLICATION ";
+    cout << "\n-----------------------------------------\n";
+    cout << "Input 'help' for commands\n";
+    
+
+    std::vector<WeatherData> weatherDataVector;
+
+    weatherDataVector.push_back({ 0, "TestLocation", "50", "76" });
+    weatherDataVector.push_back({ 1, "TestLocationAnother", "24", "32" });
+
+
+
+    bool UserCommand = true;
+
+    while(UserCommand == true)
+    {
+        string command;
+        cin >> command;
+
+        std::istringstream iss(command);
+        string input;
+        std::vector<std::string> inputs;
+
+        while (iss >> input) {
+            inputs.push_back(input);
+        }
+
+
+
+
+        
+        if (find(commands.begin(), commands.end(), inputs[0]) != commands.end())
+            if (inputs[0] == "help") {
+                cout << "Here are help commands\n";
+            }
+            else if (inputs[0] == "ShowAll") {
+                for (int i = 0; i < weatherDataVector.size(); i++) {
+                    printWeatherData(weatherDataVector, i);
+                }
+            }
+            else if (inputs[0] == "TestAPI") {
+                printWeatherData(getWeatherData("52.52", "13.419998"), 0);//TestAPI 52.52 13.419998
+            }
+            else if (inputs[0] == "quit") {
+                break;
+            }
+            else {
+                cout << "other commands\n";
+            }
+        else
+            cout << "Command invalid\n";
+
+    
+    };
+
+
+    return 0;
+
+    //location id, location name, latitude, longitude
+    
+    //Hourly ((temperature, precipitation, pressure, wind, humidity, and cloudiness)
+    // solar radiation, pressure levels
+
+}
+
 
---
+++ vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/x64-windows-dbg/include/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/pkgs/jsoncpp_x64-windows/debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "OFF")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/json" TYPE FILE FILES
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/allocator.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/assertions.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/config.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/forwards.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/json.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/json_features.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/reader.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/value.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/version.h"
+    "D:/03ben/Documents/Programming Projects/SDICoursework/vcpkg_installed/x64-windows/vcpkg/blds/jsoncpp/src/1.9.5-13b47286ba.clean/include/json/writer.h"
+    )
+endif()
+
