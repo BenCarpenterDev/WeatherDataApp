e4c9c1c22702ea18668e19da8998f4d542fa57e3
bcarpenter03 <03benjamincarpenter@gmail.com>
2024-04-14 17:52:21 +0100

    Added add, remove and search functionality

--- SDI Coursework.cpp
+++ SDI Coursework.cpp
@@ -15,24 +15,32 @@ using namespace std;
 
 struct WeatherData {// for storing weatherdata in a vector
     int locationID;
-    std::string locationName;
-    std::string latitude;
-    std::string longitude;
+    string locationName;
+    string latitude;
+    string longitude;
 
     //hourly variables
-    std::string temperature;
-    std::string relativeHumidity;
-    std::string precipitation;
+    string time;
+    string temperature;
+    string relativeHumidity;
+    string precipitation;
 
-    std::string sealevelPressure;
-    std::string surfacePressure;
-    std::string cloudCover;
+    string sealevelPressure;
+    string surfacePressure;
+    string cloudCover;
 
-    std::string windSpeed10;
+    string windSpeed10;
     /*
     std::string windSpeed80;
     std::string windSpeed120;
     std::string windSpeed180;*/
+};
+
+struct geoData {
+    int geoID;
+    string geoName;
+    string geoLat;
+    string geoLon;
 };
 
 void printWeatherData(const std::vector<WeatherData>& dataVector, int index) {
@@ -41,6 +49,7 @@ void printWeatherData(const std::vector<WeatherData>& dataVector, int index) {
     cout << "\nLatitude: " << dataVector[index].latitude;
     cout << "\nLongitude: " << dataVector[index].longitude;
 
+    cout << "\nTime by Hour: " << dataVector[index].time;
     cout << "\nTemperature: " << dataVector[index].temperature;
     cout << "\nRelative Humidity: " << dataVector[index].relativeHumidity;
     cout << "\nPrecipitation: " << dataVector[index].precipitation;
@@ -49,21 +58,74 @@ void printWeatherData(const std::vector<WeatherData>& dataVector, int index) {
     cout << "\nSurface Pressure: " << dataVector[index].surfacePressure;
     cout << "\nCloud Cover: " << dataVector[index].cloudCover;
     cout << "\nWind Speed at 10 metres: " << dataVector[index].windSpeed10;
-    cout << "\n";
+    cout << "\n \n";
 };
 
-vector<string> commands{ "quit", "help", "addLocation", "removeLocation", "modifyLocation", "searchLocation", "favouriteLocation", "ShowAll", "TestAPI"};
+vector<string> commands{ "quit", "help", "add", "remove", "modify", "search", "saved"};
 
 size_t writeCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
     output->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
 
-std::vector<WeatherData> getWeatherData(string latitude, string longitude) {
+std::vector<geoData> getGeoData(string searchStr) {
     CURL* curl;
     CURLcode res;
     string responseBuffer;
 
+    curl = curl_easy_init();
+    if (curl) {
+        // Set the URL you want to request
+        string apiKey = "YOUR_API_KEY"; // Replace with your Open-Meteo API key
+        string url = "https://geocoding-api.open-meteo.com/v1/search?name=" + searchStr + "&count=10&language=en&format=json";
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+        else {
+            Json::Value root;
+            Json::Reader reader;
+            bool parsingSuccessful = reader.parse(responseBuffer, root);
+            if (parsingSuccessful) {
+                // Weather Attributes
+
+                string geoID = root["results"][0]["id"].asString();
+                int id = stoi(geoID);
+                string name = root["results"][0]["name"].asString();
+                string lat = root["results"][0]["latitude"].asString();
+                string lon = root["results"][0]["longitude"].asString();
+
+
+                std::vector<geoData> TempVector;
+
+                TempVector.push_back({ id, name, lat, lon });
+
+                return TempVector;
+            }
+        }
+        curl_easy_cleanup(curl);
+    }
+}
+
+std::vector<WeatherData> getWeatherData(string name) {
+    std::vector<geoData> geoVector;
+    geoVector = getGeoData(name);
+
+    string latitude = geoVector[0].geoLat;
+    string longitude = geoVector[0].geoLon;
+    string locationName = geoVector[0].geoName;
+    int id = geoVector[0].geoID;
+
+
+    CURL* curl;
+    CURLcode res;
+    string responseBuffer;
 
     curl = curl_easy_init();
     if (curl) {
@@ -75,14 +137,12 @@ std::vector<WeatherData> getWeatherData(string latitude, string longitude) {
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
 
-
         res = curl_easy_perform(curl);
 
         if (res != CURLE_OK) {
             std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
         }
         else {
-
             Json::Value root;
             Json::Reader reader;
             bool parsingSuccessful = reader.parse(responseBuffer, root);
@@ -92,6 +152,7 @@ std::vector<WeatherData> getWeatherData(string latitude, string longitude) {
                 string weatherLatitude = root["latitude"].asString();
                 string weatherLongitude = root["longitude"].asString();
 
+                string hourlyTime = root["hourly"]["time"][0].asString();
                 string hourlyTemperature = root["hourly"]["temperature_2m"][0].asString();
                 string hourlyHumidity = root["hourly"]["relative_humidity_2m"][0].asString();
                 string hourlyPrecipitation = root["hourly"]["precipitation"][0].asString();
@@ -102,35 +163,28 @@ std::vector<WeatherData> getWeatherData(string latitude, string longitude) {
 
                 std::vector<WeatherData> TempVector;
 
-                TempVector.push_back({ 2, "Nottingham", weatherLatitude, weatherLongitude, hourlyTemperature,
+                TempVector.push_back({ id, locationName, weatherLatitude, weatherLongitude, hourlyTime, hourlyTemperature,
                     hourlyHumidity, hourlyPrecipitation, hourlySeaPressure, hourlySurfacePressure, hourlyCloudCover, hourlyWindSpeed });
 
                 return TempVector;
-
-            }
-            else {
-                cerr << "Failed to parse JSON response" << std::endl;
             }
         }
-
         curl_easy_cleanup(curl);
     }
-    else {
-        std::cerr << "Failed to initialize libcurl" << std::endl;
-    }
-
 }
 
+
+
 int main() {
-    cout << "WELCOME TO BEN'S WEATHER DATA APPLICATION ";
+    cout << "| WELCOME TO BEN'S WEATHER DATA APPLICATION |";
     cout << "\n-----------------------------------------\n";
-    cout << "Input 'help' for commands\n";
+    cout << "Input 'help' for commands\n\n";
     
 
-    std::vector<WeatherData> weatherDataVector;
+    std::vector<WeatherData> savedDataVector;
 
-    weatherDataVector.push_back({ 0, "TestLocation", "50", "76" });
-    weatherDataVector.push_back({ 1, "TestLocationAnother", "24", "32" });
+    savedDataVector.push_back({ 0, "TestLocation", "50", "76" });
+    savedDataVector.push_back({ 1, "TestLocationAnother", "24", "32" });
 
 
 
@@ -138,18 +192,16 @@ int main() {
 
     while(UserCommand == true)
     {
-        string command;
-        cin >> command;
+        string command, input;
 
-        std::istringstream iss(command);
-        string input;
-        std::vector<std::string> inputs;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<std::string> inputs;
 
-        while (iss >> input) {
-            inputs.push_back(input);
+        while (ss >> command) {
+            inputs.push_back(command);
         }
-
-
+        int id = savedDataVector.size() + 1;
 
 
         
@@ -157,14 +209,41 @@ int main() {
             if (inputs[0] == "help") {
                 cout << "Here are help commands\n";
             }
-            else if (inputs[0] == "ShowAll") {
-                for (int i = 0; i < weatherDataVector.size(); i++) {
-                    printWeatherData(weatherDataVector, i);
+
+        // show all saved/favourited locations
+            else if (inputs[0] == "saved") {
+                cout << "\n| SHOWING FAVOURITED LOCATIONS |";
+                cout << "\n-----------------------------------------\n";
+
+                for (int i = 0; i < savedDataVector.size(); i++) {
+                    printWeatherData(savedDataVector, i);
                 }
             }
-            else if (inputs[0] == "TestAPI") {
-                printWeatherData(getWeatherData("52.52", "13.419998"), 0);//TestAPI 52.52 13.419998
+        // Search and print weather data by longitude and latitude
+            else if (inputs[0] == "search") {
+                cout << "\n| SEARCH RESULTS |";
+                cout << "\n-----------------------------------------\n";
+                printWeatherData(getWeatherData(inputs[1]), 0);//search Nottingham
             }
+
+        // save locations
+            else if (inputs[0] == "add") {//add Nottingham
+                std::vector<WeatherData> temp = getWeatherData(inputs[1]);
+                savedDataVector.insert(savedDataVector.end(), temp.begin(), temp.end());
+                cout << "\n| "+ inputs[1] +" LOCATION FAVOURITED |";
+                cout << "\n-----------------------------------------\n";
+            }
+
+        // remove saved locations
+            else if (inputs[0] == "remove") {//remove Nottingham
+
+                savedDataVector.erase(remove_if(savedDataVector.begin(), savedDataVector.end(),[&](const WeatherData& data) { return data.locationName == inputs[1]; }),savedDataVector.end());
+
+                cout << "\n| " + inputs[1] + " LOCATION REMOVED |";
+                cout << "\n-----------------------------------------\n";
+            }
+
+        // quit program
             else if (inputs[0] == "quit") {
                 break;
             }
